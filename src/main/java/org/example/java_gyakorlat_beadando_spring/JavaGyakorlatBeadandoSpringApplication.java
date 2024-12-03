12770d96bf2ab6258fccd98b2140b32f6f83e424
Beantech0 <holyba.attila@gmail.com>
2024-12-03 07:05:09 +0100

    futtat.jar fix

--- src/main/java/org/example/java_gyakorlat_beadando_spring/JavaGyakorlatBeadandoSpringApplication.java
+++ src/main/java/org/example/java_gyakorlat_beadando_spring/JavaGyakorlatBeadandoSpringApplication.java
@@ -9,5 +9,4 @@ public class JavaGyakorlatBeadandoSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaGyakorlatBeadandoSpringApplication.class, args);
     }
-
 }
